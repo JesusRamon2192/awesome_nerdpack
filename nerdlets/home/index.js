d890a38316fbcde729a62625050658f74f61f742
JesusPCWin <jesus.ramon2192@gmail.com>
2024-12-03 16:53:43 -0600

    segundo commit

--- nerdlets/home/index.js
+++ nerdlets/home/index.js
@@ -16,39 +16,51 @@ const ACCOUNT_ID = 2132798
 const VERSION_A_DESCRIPTION = 'The newsletter signup message says, "Sign up for our newsletter"'
 const VERSION_B_DESCRIPTION = 'The newsletter signup message says, "Sign up for our newsletter and get a free shirt!"'
 
-export default class HomeNerdlet extends React.Component {
+export default class AbTestNerdletNerdlet extends React.Component {
   render() {
-    return <div>
-      <Grid className="wrapper">
-        <GridItem columnSpan={6}>
-          <VersionDescription
-            description={VERSION_A_DESCRIPTION}
-            version="A"
-          />
-        </GridItem>
-        <GridItem columnSpan={6}>
-          <VersionDescription
-            description={VERSION_B_DESCRIPTION}
-            version="B"
-          />
-        </GridItem>
-        <GridItem columnSpan={12}><NewsletterSignups /></GridItem>
-        <GridItem columnSpan={6}><TotalSubscriptions /></GridItem>
-        <GridItem columnSpan={6}><TotalCancellations /></GridItem>
-        <GridItem columnSpan={6}><VersionTotals version='a' accountIds={ACCOUNT_ID} /></GridItem>
-        <GridItem columnSpan={6}><VersionTotals version='b' accountIds={ACCOUNT_ID} /></GridItem>
-        <ChartGroup>
-          <GridItem columnSpan={6}>
-            <VersionPageViews version='a' />
-          </GridItem>
-          <GridItem columnSpan={6}>
-            <VersionPageViews version='b' />
-          </GridItem>
-        </ChartGroup>
-        <GridItem columnSpan={12}><EndTestSection /></GridItem>
-        <GridItem columnSpan={12}><PastTests /></GridItem>
-      </Grid>
-    </div>
-      ;
+      return <div>
+          <Grid className="wrapper">
+              <GridItem columnSpan={6}>
+                  <VersionDescription
+                      description={VERSION_A_DESCRIPTION}
+                      version="A"
+                  />
+              </GridItem>
+              <GridItem columnSpan={6}>
+                  <VersionDescription
+                      description={VERSION_B_DESCRIPTION}
+                      version="B"
+                  />
+              </GridItem>
+              <GridItem columnSpan={12}><hr /></GridItem>
+              <GridItem columnSpan={12}><NewsletterSignups /></GridItem>
+              <GridItem columnSpan={6}><TotalSubscriptions /></GridItem>
+              <GridItem columnSpan={6}><TotalCancellations /></GridItem>
+              <GridItem columnSpan={6}>
+                  <VersionTotals version='a' accountIds={ACCOUNT_ID} />
+              </GridItem>
+              <GridItem columnSpan={6}>
+                  <VersionTotals version='b' accountIds={ACCOUNT_ID} />
+              </GridItem>
+              <ChartGroup>
+                  <GridItem columnSpan={6}>
+                      <VersionPageViews version='a' />
+                  </GridItem>
+                  <GridItem columnSpan={6}>
+                      <VersionPageViews version='b' />
+                  </GridItem>
+              </ChartGroup>
+              <GridItem columnSpan={12}>
+                  <EndTestSection
+                      accountIds={ACCOUNT_ID}
+                      versionADescription={VERSION_A_DESCRIPTION}
+                      versionBDescription={VERSION_B_DESCRIPTION}
+                  />
+              </GridItem>
+              <GridItem columnSpan={12}>
+                  <PastTests accountIds={ACCOUNT_ID} />
+              </GridItem>
+          </Grid>
+      </div>
   }
 }
