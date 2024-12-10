4507e1d4dc16bc7910a81fb0c7e8cf0ff7f86b72
JesusPCWin <jesus.ramon2192@gmail.com>
2024-12-10 17:35:04 -0600

    Navegacion to apm

--- nerdlets/line-chart/newsletter-signups.js
+++ nerdlets/line-chart/newsletter-signups.js
@@ -1,27 +1,43 @@
 import React from 'react';
 import {
+    Button,
     HeadingText,
     LineChart,
     NrqlQuery,
     PlatformStateContext,
+    Stack,
+    StackItem,
+    navigation,
 } from 'nr1';
 
 const ACCOUNT_ID = 2132798
+const ENTITY_GUID = "MjEzMjc5OHxBUE18QVBQTElDQVRJT058MTA3Nzk1NTQ5Ng"
 
 export default class NewsletterSignups extends React.Component {
+    openAPMEntity() {
+        navigation.openStackedEntity(ENTITY_GUID)
+    }
+
     render() {
         return <div>
-            <HeadingText className="chartHeader">
-                Newsletter subscriptions per version
-            </HeadingText>
-
+            <Stack fullWidth>
+                <StackItem grow={true}>
+                    <HeadingText className="chartHeader">
+                        Newsletter subscriptions per version
+                    </HeadingText>
+                </StackItem>
+                <StackItem>
+                    <Button onClick={this.openAPMEntity}>
+                        App performance
+                    </Button>
+                </StackItem>
+            </Stack>
             <PlatformStateContext.Consumer>
                 {
                     (platformState) => {
-                        //console.log(platformState.timeRange);
                         return <NrqlQuery
                             accountIds={[ACCOUNT_ID]}
-                            query="SELECT count(*) FROM subscription FACET page_version SINCE 30 MINUTES AGO TIMESERIES"
+                            query="SELECT count(*) FROM subscription FACET page_version TIMESERIES"
                             timeRange={platformState.timeRange}
                             pollInterval={60000}
                         >
